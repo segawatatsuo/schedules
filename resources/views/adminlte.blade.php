ba46942bd83b8e88ddc9736899e9c4d452b80a87
“segawa” <segawa@lookingfor.jp>
2023-10-29 09:56:43 +0900

    10-29 09:57

--- resources/views/adminlte.blade.php
+++ resources/views/adminlte.blade.php
@@ -10,17 +10,9 @@
 <div class="col-md-12">
     <div class="card card-primary">
         <div class="card-body p-0">
-            <div id="app">
-                <div class="m-auto w-100 m-5 p-5">
-                    <div id='calendar'></div>
-                </div>
-            </div>
-    
-            <link href='{{ asset('fullcalendar-6.1.9/lib/main.css') }}' rel='stylesheet' />
+            <div id='calendar'></div>
         </div>
-
     </div>
-
 </div>
 
 @stop
@@ -32,33 +24,9 @@
 @stop
 
 @section('js')
-    <script> console.log('ページごとJSの記述'); </script>
+
     <script src="{{ mix('js/app.js') }}"></script>
 
-    <script src='{{ asset('fullcalendar-6.1.9/lib/main.js') }}'></script>
-    
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            var calendarEl = document.getElementById('calendar');
-            var calendar = new FullCalendar.Calendar(calendarEl, {
-                initialView: 'dayGridMonth',
-                locale: 'ja',
-                height: 'auto',
-                firstDay: 1,
-                headerToolbar: {
-                    left: "dayGridMonth,listMonth",
-                    center: "title",
-                    right: "today prev,next"
-                },
-                buttonText: {
-                    today: '今月',
-                    month: '月',
-                    list: 'リスト'
-                },
-                noEventsContent: 'スケジュールはありません',
-             });
-             calendar.render();
-        });
-    </script>
+
 
 @stop
